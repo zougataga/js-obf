b77ebf08af5e164bddca561c8d4360cf15e8f635
zougataga <107312482+zougataga@users.noreply.github.com>
2022-11-13 08:57:52 +0100

    Delete assets directory

--- assets/function/base64.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const base64 = async (file) => {
-    var bitmap = fs.readFileSync(file);
-    return String(new Buffer(bitmap).toString('base64'));
-};
-module.exports = base64;
--- assets/function/size.js
+++
@@ -1,5 +0,0 @@
-const size = (size) => {
-    const i = Math.floor(Math.log(size) / Math.log(1024));
-    return String((size / Math.pow(1024, i)).toFixed(2) * 1 + " " + ['B', 'KB', 'MB', 'GB', 'TB'][i])
-};
-module.exports = size;
